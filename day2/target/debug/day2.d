333773c605c8e9fc120eb6359730ada6c9538f7e
1024小神 <15670339118@163.com>
2024-02-04 20:13:07 +0800

    add

---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+D:\Mypro\rustLearn\day2\target\debug\day2.exe: D:\Mypro\rustLearn\day2\src\main.rs
---
+++ day2/target/debug/deps/day2.d
@@ -0,0 +1,5 @@
+D:\Mypro\rustLearn\day2\target\debug\deps\day2.exe: src\main.rs
+
+D:\Mypro\rustLearn\day2\target\debug\deps\day2.d: src\main.rs
+
+src\main.rs:
